64669fa7855c69f41c6ab95509aab6721936ff07
Adam Byrne <adam.r.byrne@gmail.com>
2025-02-22 14:17:37 +0000

    concurrent server behind electron

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { CONFIG } from "./config";
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,8 +10,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Express REST!");
 });
 
-// Start server on port 4000
-const PORT = 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(CONFIG.SERVER_PORT, () => {
+  console.log(`Server running on ${CONFIG.SERVER_URL}`);
 });
